111222d3e822d6bc7dbae5a90123847d3533ebf9
damlakayali <damlakayali@gmail.com>
2023-01-03 15:13:57 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Category:IEntity
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int createdById { get; set; }
+        public DateTime createdTime { get; set; }
+        public int lastUpdatedById { get; set; }
+        public DateTime lastUpdatedTime { get; set; }
+
+    }
+}
