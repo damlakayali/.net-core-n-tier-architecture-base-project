8e2303ac63a040775fc0c983fade5a0fb55086c6
damlakayali <damlakayali@gmail.com>
2023-01-04 01:00:49 +0300

    add core layer

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity,TContext>
+        where TEntity : class,IEntity,new()
+        where TContext: DbContext,new()
+    {
+    }
+}
